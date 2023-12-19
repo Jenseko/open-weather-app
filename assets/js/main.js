508f4492b5e42a5c5ba467788250c6a882d527cb
Jenseko <jenscode23@gmail.com>
2023-12-19 23:55:08 +0100

    fix the card header date

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ btnStart.addEventListener("click", getWeatherData);
 btnRefresh.addEventListener("click", refreshPage);
 
 // Define the function for the API request
-function getWeatherData() {
+async function getWeatherData() {
   // Get article element for check of text content
 
   const textContentArticle = article.textContent;
@@ -26,39 +26,37 @@ function getWeatherData() {
     const weatherDataArr = [];
 
     // Call the fetch function to request the desired weather data utilizing the current weather data API of open weather
-    fetch(
+
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&units=metric&appid=2fa1662c1af641d6fd162fc1fe68f4f5`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Utilizing object destructuring to filter the desired data from the weather API
-        const { main, weather, dt, timezone, wind, clouds, sys, coord } = data;
+    );
+    const data = await response.json();
+    // Utilizing object destructuring to filter the desired data from the weather API
+    console.log(data);
+    const { main, weather, dt, timezone, wind, clouds, sys, coord } = data;
 
-        // Create array with extracted key-value-pairs for the card body
-        const weatherDataHeader = [
-          { Temperature: `${main.temp.toFixed(1)} °C` },
-          { Description: weather[0].description },
-          { "Data obtained": obtainedTime(dt) },
-        ];
+    // Create array with extracted key-value-pairs for the card body
+    const weatherDataHeader = [
+      { Temperature: `${main.temp.toFixed(1)} °C` },
+      { Description: weather[0].description },
+      { "Data obtained": obtainedTime(dt) },
+    ];
 
-        // Create array with extracted key-value-pairs for the card header
-        const weatherDataBody = [
-          { "Locale Time": `${localeTime(timezone)}` },
-          { "Wind Speed": `${wind.speed} m/s` },
-          { Cloudiness: `${clouds.all} %` },
-          { Pressure: `${main.pressure} hpa` },
-          { Humidity: `${main.humidity} %` },
-          { Sunrise: `${convertTime(sys.sunrise)} Uhr` },
-          { Sunset: `${convertTime(sys.sunset)} Uhr` },
-          {
-            Coordinates: `[ ${coord.lon.toFixed(2)} , ${coord.lat.toFixed(
-              2
-            )} ]`,
-          },
-        ];
+    // Create array with extracted key-value-pairs for the card header
+    const weatherDataBody = [
+      { "Locale Time": `${localeTime(timezone)}` },
+      { "Wind Speed": `${wind.speed} m/s` },
+      { Cloudiness: `${clouds.all} %` },
+      { Pressure: `${main.pressure} hpa` },
+      { Humidity: `${main.humidity} %` },
+      { Sunrise: `${convertTime(sys.sunrise)} Uhr` },
+      { Sunset: `${convertTime(sys.sunset)} Uhr` },
+      {
+        Coordinates: `[ ${coord.lon.toFixed(2)} , ${coord.lat.toFixed(2)} ]`,
+      },
+    ];
 
-        createHtmlCard(weatherDataHeader, weatherDataBody);
-      });
+    createHtmlCard(weatherDataHeader, weatherDataBody);
   }
 }
 
@@ -81,11 +79,11 @@ function createHtmlCard(weatherDataHeader, weatherDataBody) {
 
   // Get the table element to fill it with data
   const weatherDataArt = document.querySelector(".grid-container");
-  // console.log(tableData);
+  console.log(weatherDataArt);
 
   // Display the data body inside a grid-container
   weatherDataBody.forEach((data) => {
-    weatherDataArt.innerHTML += `<p>${Object.keys(data)[0]} : \n ${
+    weatherDataArt.innerHTML += `<p>${Object.keys(data)[0]}: <br>${
       Object.values(data)[0]
     }</p>`;
   });
@@ -94,17 +92,14 @@ function createHtmlCard(weatherDataHeader, weatherDataBody) {
 // Function to calculate the obtained time of the data by converting the unix time and convert it into european time format
 function obtainedTime(unixTime) {
   const obtain = new Date(unixTime * 1000);
-  const obtainNew =
-    "Obtained at " +
-    obtain.getHours() +
-    ":" +
-    obtain.getMinutes() +
-    " Uhr, " +
-    obtain.getDate() +
-    "." +
-    (obtain.getMonth() + 1) +
-    "." +
-    obtain.getFullYear();
+
+  const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const day = weekdays[obtain.getDay()];
+  const month = (obtain.getMonth() + 1).toString().padStart(2, 0);
+
+  const obtainNew = `Obtained at
+    ${obtain.getHours()}:${obtain.getMinutes()} Uhr <br>${day}
+    ${obtain.getDate()}/${month}/${obtain.getFullYear()}`;
   return obtainNew;
 }
 
